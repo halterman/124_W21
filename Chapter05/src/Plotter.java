6bdb5f43fcd9bf2493a76b0e6c0ffa98547399a3
Rick Halterman <halterman@southern.edu>
2021-03-10 22:41:32 -0500

    Version: Wed Mar 10 22:41:31 EST 2021

--- Chapter05/src/Plotter.java
+++ Chapter05/src/Plotter.java
@@ -8,7 +8,7 @@ import javax.swing.JPanel;
 /**
  * Plots trend data over time.
  * @author Rick Halterman
- * #date 2021-03-10
+ * @date 2021-03-10
  */
 public class Plotter {
     
@@ -54,8 +54,8 @@ public class Plotter {
             int height = getHeight();
             int width = getWidth();
             // Compute the vertical size of the data
-            double min = minimum(dataSet) - 1;
-            double max = maximum(dataSet) + 1;
+            double min = minimum(dataSet);
+            double max = maximum(dataSet);
             double dataHeight = max - min; //range(dataSet); 
             double widthScale = (double) width/(dataSet.length - 1);
             double heightScale = height/dataHeight;
@@ -67,10 +67,22 @@ public class Plotter {
             // Plot data
             g.setColor(Color.RED);
             for (int i = 1; i < dataSet.length; i++) {
-                g.drawLine((int) Math.round((i - 1) * widthScale), 
-                           height - (int) Math.round((dataSet[i - 1] - min) * heightScale),
-                           (int) Math.round(i * widthScale), 
-                           height - (int) Math.round((dataSet[i] - min) * heightScale));
+                int x1 = (int) Math.round((i - 1) * widthScale), 
+                    y1 = height - (int) Math.round((dataSet[i - 1] - min) * heightScale),
+                    x2 = (int) Math.round(i * widthScale), 
+                    y2 = height - (int) Math.round((dataSet[i] - min) * heightScale);
+                if (y1 < 1) {
+                    y1 = 1;
+                }
+                if (y2 < 1) {
+                    y2 = 1;
+                }
+                g.drawLine(x1, y1, x2, y2);
+//                Original line drawing:
+//                g.drawLine((int) Math.round((i - 1) * widthScale), 
+//                           height - (int) Math.round((dataSet[i - 1] - min) * heightScale),
+//                           (int) Math.round(i * widthScale), 
+//                           height - (int) Math.round((dataSet[i] - min) * heightScale));
             }
         }
     }
@@ -80,16 +92,33 @@ public class Plotter {
      * with a given size.  The plot fills the window horizontally
      * and vertically.
      * @param data the data set to plot
+     * @param left the <em>x</em> position of the plot window's left-top corner
+     * @param top the <em>y</em> position of the plot window's left-top corner
+     * @param width the width of the graphical window, in pixels
      * @param width the width of the graphical window, in pixels
      * @param height the height of the graphical window, in pixels
      */
-    public static void plot(double[] data, int width, int height) {
+    public static void plot(double[] data, int left, int top, int width, int height) {
         JFrame window = new JFrame("Plot");
         JPanel panel = new PlotPanel(data, width, height);
         window.add(panel);
         window.pack();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLocation(left, top);
         window.setVisible(true);
+    }
+    
+    
+    /**
+     * Plots the data stored in an array in a graphical window
+     * with a given size.  The plot fills the window horizontally
+     * and vertically.
+     * @param data the data set to plot
+     * @param width the width of the graphical window, in pixels
+     * @param height the height of the graphical window, in pixels
+     */
+    public static void plot(double[] data, int width, int height) {
+        plot(data, 50, 50, width, height);
     }
     
     /**
@@ -101,6 +130,20 @@ public class Plotter {
      * @param height the height of the graphical window, in pixels
      */
     public static void plot(int[] data, int width, int height) {
+        plot(data, 100, 50, width, height);
+    }
+
+    /**
+     * Plots the data stored in an array in a graphical window
+     * with a given size.  The plot fills the window horizontally
+     * and vertically.  This overloaded method accepts an array of integers.
+     * @param data the data set to plot
+     * @param left the <em>x</em> position of the plot window's left-top corner
+     * @param top the <em>y</em> position of the plot window's left-top corner
+     * @param width the width of the graphical window, in pixels
+     * @param height the height of the graphical window, in pixels
+     */
+    public static void plot(int[] data, int left, int top, int width, int height) {
         // Works, but not as efficient
         //plot(Arrays.stream(data).asDoubleStream().toArray(), width, height);
         
@@ -109,6 +152,6 @@ public class Plotter {
         for (int i = 0; i < doubleData.length; i++) {
             doubleData[i] = data[i];
         }
-        plot(doubleData, width, height);
+        plot(doubleData, left, top, width, height);
     }
 }
